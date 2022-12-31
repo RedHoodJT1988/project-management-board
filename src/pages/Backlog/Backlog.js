cd2c6bd6603fa61a3be518132d04f25bd5fef162
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-12-31 10:49:58 -0800

    finish project management board

---
+++ src/pages/Backlog/Backlog.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import Task from '../../components/Task/Task';
+import useDataFetching from '../../hooks/useDataFetching';
+
+const BacklogWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 5%;
+`;
+
+const Title = styled.h2`
+    width: 100%;
+    padding-bottom: 10px;
+    text-align: center;
+    border-bottom: 1px solid darkGray;
+`;
+
+const TasksWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 5%;
+`;
+
+function Backlog() {
+    const [loading, error, tasks] = useDataFetching('https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks');
+    return (
+        <BacklogWrapper>
+            <Title>Backlog</Title>
+            <TasksWrapper>
+                {loading || error ? (
+                    <span>{error || 'Loading...'}</span>
+                ) : (
+                    tasks.map((task) => (
+                        <Task
+                            key={task.id}
+                            title={task.title}
+                            body={task.body}
+                        />
+                    ))
+                )}
+            </TasksWrapper>
+        </BacklogWrapper>
+    );
+}
+
+export default Backlog;
---
+++ src/pages/Board/Board.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import useDataFetching from '../../hooks/useDataFetching';
+import Lane from '../../components/Lane/Lane';
+
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 5%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const lanes = [
+  { id: 1, title: 'To Do' },
+  { id: 2, title: 'In Progress' },
+  { id: 3, title: 'Review' },
+  { id: 4, title: 'Done' },
+];
+
+function onDragStart(e, id) {
+  e.dataTransfer.setData('id', id);
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+}
+
+function Board() {
+  const [loading, error, data] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, [data]);
+
+  function onDrop(e, laneId) {
+    const id = e.dataTransfer.getData('id');
+    const updatedTasks = tasks.filter((task) => {
+      if (task.id.toString() === id) {
+        task.lane = laneId;
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <BoardWrapper>
+      {lanes.map((lane) => (
+        <Lane 
+          key={lane.id}
+          laneId={lane.id}
+          title={lane.title} 
+          loading={loading}
+          error={error}
+          tasks={tasks.filter((task) => task.lane === lane.id)}
+          onDragStart={onDragStart}
+          onDragOver={onDragOver}
+          onDrop={onDrop}
+        />
+      ))}
+    </BoardWrapper>
+  );
+}
+
+export default Board;
